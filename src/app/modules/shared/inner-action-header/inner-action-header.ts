8d100cb52e5e81aeac35737a49e538928cf73be4
Butalib <1p2o3p4p5o6p@gmail.com>
2026-08-03 09:26:34 +0300

    feat: implement inner action header with search and action functionalities

--- src/app/modules/shared/inner-action-header/inner-action-header.ts
+++ src/app/modules/shared/inner-action-header/inner-action-header.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-inner-action-header',
@@ -6,4 +8,46 @@ import { Component } from '@angular/core';
   templateUrl: './inner-action-header.html',
   styleUrl: './inner-action-header.scss',
 })
-export class InnerActionHeader {}
+export class InnerActionHeader implements OnInit, OnDestroy {
+  // ui elment render 
+  @Input() showSearch: boolean = true;
+  @Input() showActionButton: boolean = false;
+  @Input() showFilterButton: boolean = false;
+  @Input() showDate: boolean = false;
+  @Input() actionLabel: string = '';
+  @Input() actionIcon: string = 'fa-solid fa-plus';
+  @Input() dateLabel: string = '';
+  @Input() searchPlaceholder: string = 'ابحث هنا...';
+  // ui event 
+  @Output() search = new EventEmitter<string>();
+  @Output() actionClicked = new EventEmitter<void>();
+  @Output() filterClicked = new EventEmitter<void>();
+  //event handlers
+  onSearch(value: string) {
+    this.search.emit(value);
+  }
+  onActionClicked() {
+    this.actionClicked.emit();
+  }
+  onFilterClicked() {
+    this.filterClicked.emit();
+  }
+  // 1. تعريف الـ FormControl للسيرش
+  searchControl = new FormControl('');
+  private destroy$ = new Subject<void>();
+  ngOnInit() {
+    // 2. تطبيق الـ Debounce
+    this.searchControl.valueChanges.pipe(
+      debounceTime(500), // استنى نص ثانية بعد آخر حرف
+      distinctUntilChanged(), // متكررش الريكوست لنفس الكلمة
+      takeUntil(this.destroy$) // لمنع تسريب الذاكرة (Memory Leak)
+    ).subscribe(value => {
+      console.log('Search Value:', value);
+      this.search.emit(value || '');
+    });
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
--- src/app/modules/shared/shared-module.ts
+++ src/app/modules/shared/shared-module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing-module';
 import { InnerActionHeader } from './inner-action-header/inner-action-header';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [InnerActionHeader],
-  imports: [CommonModule, SharedRoutingModule],
+  imports: [CommonModule, SharedRoutingModule , ReactiveFormsModule],
   exports: [InnerActionHeader],
 })
 export class SharedModule {}
